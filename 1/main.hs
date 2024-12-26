e9f9966d5f3ac238c5edb3a54616660a9b08248c
Roel de Jong <12800443+twiggler@users.noreply.github.com>
2024-12-26 09:59:59 +0100

    Puzzle 2

--- 1/main.hs
+++ 1/main.hs
@@ -1,26 +1,26 @@
-import System.Environment  
 import Data.List
-import qualified Data.Map.Strict as Map
+import Data.Map.Strict qualified as Map
+import System.Environment
 
 readLocationFile :: String -> IO ([Int], [Int])
-readLocationFile filename = do 
-    rows <- map words . lines <$> readFile filename
-    let parsedRows = [(read loc1, read loc2) | [loc1, loc2] <- rows] 
-    return (unzip parsedRows)
+readLocationFile filename = do
+  rows <- map words . lines <$> readFile filename
+  let parsedRows = [(read loc1, read loc2) | [loc1, loc2] <- rows]
+  return (unzip parsedRows)
 
 totalDistance :: [Int] -> [Int] -> Int
 totalDistance xss yss = sum $ zipWith (\x y -> abs (x - y)) xss yss
 
 similarityScore :: [Int] -> [Int] -> Int
 similarityScore xss yss = sum [x * Map.findWithDefault 0 x freqMap | x <- xss]
-    where freqMap = Map.fromAscListWith (+) [(y, 1) | y <- yss]
+  where
+    freqMap = Map.fromAscListWith (+) [(y, 1) | y <- yss]
 
 main = do
-    (locationFilePath:_) <- getArgs
-    (xs, ys) <- readLocationFile locationFilePath
-    let 
-        (xss, yss) = (sort xs, sort ys)
-        dist = totalDistance xss yss
-        similarity = similarityScore xss yss
-    putStrLn $ "Total distance equals " ++ show dist
-    putStrLn $ "Similarity score equals " ++ show similarity
+  (locationFilePath : _) <- getArgs
+  (locs1, locs2) <- readLocationFile locationFilePath
+  let (sorted_locs1, sorted_locs2) = (sort locs1, sort locs2)
+      dist = totalDistance sorted_locs1 sorted_locs2
+      similarity = similarityScore sorted_locs1 sorted_locs2
+  putStrLn $ "Total distance equals " ++ show dist
+  putStrLn $ "Similarity score equals " ++ show similarity
---
+++ 2/main.hs
@@ -0,0 +1,41 @@
+import Data.List
+import System.Environment
+
+readReportsFile :: String -> IO [[Int]]
+readReportsFile filename = map (map read . words) . lines <$> readFile filename
+
+isSortedBy :: (Int -> Int -> Bool) -> [Int] -> Bool
+isSortedBy _ [] = True
+isSortedBy _ [_] = True
+isSortedBy pred xs = and $ zipWith pred xs (tail xs)
+
+allIncreasing = isSortedBy (<)
+
+allDecreasing = isSortedBy (>)
+
+leaveOneOut :: [Int] -> [[Int]]
+leaveOneOut xs = zipWith (++) (inits xs) (tail $ tails xs)
+
+isSafeLevelDifference :: Int -> Bool
+isSafeLevelDifference x = abs x >= 1 && abs x <= 3
+
+isSafeReport :: [Int] -> Bool
+isSafeReport [] = False
+isSafeReport [_] = False -- Requirements are ambiguous, assuming that a report with only one reading is not safe
+isSafeReport levels = all isSafeLevelDifference diffs && (allIncreasing levels || allDecreasing levels)
+  where
+    diffs = zipWith (-) (tail levels) levels
+
+isSafeReportWithProblemDamper :: [Int] -> Bool
+isSafeReportWithProblemDamper levels = any isSafeReport $ leaveOneOut levels
+
+countSafeReports :: ([Int] -> Bool) -> [[Int]] -> Int
+countSafeReports reportValidator reports = length $ filter reportValidator reports
+
+main = do
+  (reportsFilePath : _) <- getArgs
+  reports <- readReportsFile reportsFilePath
+  let safe_reports_count = countSafeReports isSafeReport reports
+      safe_with_problem_damper_count = countSafeReports isSafeReportWithProblemDamper reports
+  putStrLn $ "Number of safe reports equals " ++ show safe_reports_count
+  putStrLn $ "Number of safe reports with problem damper equals " ++ show safe_with_problem_damper_count
