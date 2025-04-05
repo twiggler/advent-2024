7a5fc20df9b51b6234d2fe6f8d504e218789e9a8
Roel de Jong <12800443+twiggler@users.noreply.github.com>
2025-04-05 14:53:56 +0200

    Make grid an instance of  comonad

--- 6/main.hs
+++ 6/main.hs
@@ -6,9 +6,7 @@ import Cursor
 import Data.Containers.ListUtils (nubOrd, nubOrdOn)
 import Data.Foldable (asum)
 import Data.Functor
-import Data.Functor.Compose
 import Data.List (find)
-import Data.List.Infinite (Infinite ((:<)))
 import Data.List.NonEmpty (NonEmpty ((:|)), toList)
 import Data.List.NonEmpty qualified as Ne (last)
 import Data.Maybe (mapMaybe)
@@ -18,6 +16,7 @@ import Parsing
 import System.Environment
 import Text.ParserCombinators.ReadP
 import Control.Monad.Extra (unfoldM, liftMaybe)
+import Control.Comonad (extract)
 
 data Cell = Empty | Obstruction | Start deriving (Show, Eq, Ord)
 
@@ -27,16 +26,16 @@ data TaggedCell = TaggedCell
   }
   deriving (Show, Eq, Ord)
 
-data CardinalDir = North | East | South | West deriving (Eq, Show, Ord)
-
 {-
+UPDATE: reloaded.hs provides a more efficient solution.
+
 The world is modelled as an infinite grid, with a padding of Nothings.
 The guard position is not stored explicitly, but is the focus of the cursor composed with a cursor.
 The outer cursor represents the rows, the inner cursor represents the columns.
 
 Downsides of this approach: 
 - Moving the guard to a specific position, for example the start position, is clunky.
-- We need to compose the Grid with another data structure *World* to store the current direction anyway.
+- We need to compose the PaddedGrid with another data structure *World* to store the current direction anyway.
 - I suspect the performance is not optimal. This would not have been a problem if not for part 2.
 
 Alternatives:
@@ -45,7 +44,7 @@ Alternatives:
 -}
 
 data World = World
-  { grid :: Grid (Maybe TaggedCell),
+  { grid :: PaddedGrid TaggedCell,
     __ :: CardinalDir
   }
 
@@ -57,7 +56,7 @@ data Guard = Guard
 
 guard :: World -> Maybe Guard
 guard (World grid' dir') = do
-  cell' <- extractGrid grid'
+  cell' <- extract grid'
   return (Guard (tag cell') dir')
 
 turnRight :: CardinalDir -> CardinalDir
@@ -67,25 +66,12 @@ turnRight South = West
 turnRight West = North
 
 makeWorld :: [[Cell]] -> World
-makeWorld cells' = World (moveToStart (startTag tagged) $ toGrid tagged) North
+makeWorld cells' = World (moveToStart (startTag tagged) $ toPaddedGrid tagged) North
   where
     tagged = tagCells cells'
     startTag = maybe (0, 0) tag . asum . fmap (find (\it -> cell it == Start))
     moveToStart (x, y) = advance y South . advance x East
-    advance n dir' grid' = iterate (move2Guard dir') grid' !! n
-
--- Intuition: guard stays still, world moves
-move2Guard :: CardinalDir -> Grid (Maybe TaggedCell) -> Grid (Maybe TaggedCell)
-move2Guard d = Compose . moveVer d . getCompose . mapRows (moveHor d)
-  where
-    moveHor North = id
-    moveHor East = moveFwd
-    moveHor South = id
-    moveHor West = moveBwd
-    moveVer North = moveBwd
-    moveVer East = id
-    moveVer South = moveFwd
-    moveVer West = id
+    advance n dir' grid' = iterate (moveGrid (toDir dir')) grid' !! n
 
 lab :: ReadP [[Cell]]
 lab = do
@@ -102,20 +88,13 @@ tagCells = zipWith tagRow [0 ..]
     tagRow y = zipWith (tagCell y) [0 ..]
     tagCell y x cell' = TaggedCell cell' (x, y)
 
-toGrid :: [[TaggedCell]] -> Grid (Maybe TaggedCell)
-toGrid = Compose . toCur (pure Nothing) . fmap (toCur Nothing . fmap Just)
-  where
-    toCur padding [] = Cursor (pure padding) padding (pure padding)
-    toCur padding (a : as) = Cursor (pure padding) a (listToStream padding as)
-    listToStream padding = foldr (:<) (pure padding)
-
 -- Optimization: instead of moving a single cell at a time, we can raycast to the next obstacle. 
 step :: World -> Maybe World
 step (World grid' dir') = find isEmptyCell $ zipWith World candidateMoves candidateDirs
   where
     candidateDirs = take 4 (iterate turnRight dir')
-    candidateMoves = flip move2Guard grid' <$> candidateDirs
-    isEmptyCell = (Just Obstruction /=) . fmap cell . extractGrid . grid
+    candidateMoves = flip moveGrid grid' . toDir <$> candidateDirs
+    isEmptyCell = (Just Obstruction /=) . fmap cell . extract . grid
 
 patrol :: World -> NonEmpty World
 -- Track the guard position and direction using a Set to terminate at cycles.
@@ -142,12 +121,14 @@ solve2 :: NonEmpty World -> Int
 solve2 =  length . mapMaybe tryCaptureGuard . obstaclePositions
   where
     obstaclePositions = nubOrdOn (fmap position . guard) . tail . toList
-    tryCaptureGuard = guard <=< step . Ne.last . patrol . setupWorld  
+    tryCaptureGuard = guard <=< step . Ne.last . patrol . setupWorld
     setupWorld (World g d) =
       let d' = turnRight $ turnRight d
-          g' = move2Guard d' $ placeObstacle g
+          g' = moveGrid (toDir d') $ placeObstacle g
       in World g' d
     placeObstacle = updateGrid $ fmap (\tc -> tc { cell = Obstruction })
+    updateGrid f = mkGrid . updateCursor (updateCursor f) . getCursor
+    updateCursor f (Cursor bwd' cur' fwd') = Cursor bwd' (f cur') fwd'
 
 main :: IO ()
 main = do
@@ -158,4 +139,3 @@ main = do
       obstaclesWithLoop = solve2 path
   putStrLn $ "Number of distinct positions visited equals " ++ show distinctPositions
   putStrLn $ "Number of obstacles causing a loop equals  " ++ show obstaclesWithLoop
-  
--- common/Cursor.hs
+++ common/Cursor.hs
@@ -1,16 +1,28 @@
 module Cursor
   ( Cursor (..),
-    Grid,
+    Grid (..),
+    PaddedGrid,
     moveFwd,
     moveBwd,
-    mapRows,
-    extractGrid,
-    updateGrid
+    moveCursor,
+    moveGrid,
+    mkGrid,
+    toPaddedGrid,
+    toMatrix,
+    getCursor,
+    toDir,
+    Dir (..),
+    Dir2,
+    CardinalDir (..),
   )
 where
 
+import Control.Comonad
+import Data.Distributive
 import Data.Functor.Compose
-import Data.List.Infinite ( Infinite(..) )
+import Data.List.Infinite (Infinite (..))
+import Data.List.Infinite qualified as I
+import Data.Maybe (isJust, fromJust)
 
 data Cursor a = Cursor {bwd :: Infinite a, cur :: a, fwd :: Infinite a} deriving (Functor)
 
@@ -18,24 +30,64 @@ instance Applicative Cursor where
   pure a = Cursor (pure a) a (pure a)
   Cursor b1 c1 f1 <*> Cursor b2 c2 f2 = Cursor (b1 <*> b2) (c1 c2) (f1 <*> f2)
 
+instance Comonad Cursor where
+  extract (Cursor _ cur' _) = cur'
+  duplicate c = Cursor (I.iterate moveBwd (moveBwd c)) c (I.iterate moveFwd (moveFwd c))
+
+instance Distributive Cursor where
+  distribute fCur = Cursor (I.transpose (bwd <$> fCur)) (cur <$> fCur) (I.transpose (fwd <$> fCur))
+
 moveFwd, moveBwd :: Cursor a -> Cursor a
 moveFwd (Cursor bwd' cur' (fwd' :< next')) = Cursor (cur' :< bwd') fwd' next'
 moveBwd (Cursor (bwd' :< prev') cur' fwd') = Cursor prev' bwd' (cur' :< fwd')
 
--- We do not need full Comonad implementation
-extract :: Cursor a -> a
-extract (Cursor _ cur' _) = cur'
+newtype Grid a = Grid (Compose Cursor Cursor a)
 
-update :: (a -> a) -> Cursor a -> Cursor a
-update f (Cursor bwd' cur' fwd') = Cursor bwd' (f cur') fwd'
+instance Functor Grid where
+  fmap f (Grid c) = Grid (fmap f c)
 
-type Grid a = Compose Cursor Cursor a
+instance Comonad Grid where
+  extract = extract . extract . getCursor
+  duplicate =
+    fmap (Grid . Compose) . (Grid . Compose) . fmap distribute . duplicate . fmap duplicate . getCursor
 
-mapRows :: (Cursor a -> Cursor a) -> Grid a -> Grid a
-mapRows f (Compose (Cursor bwd' cur' fwd')) = Compose $ Cursor (f <$> bwd') (f cur') (f <$> fwd')
+mkGrid :: Cursor (Cursor a) -> Grid a
+mkGrid = Grid . Compose
 
-extractGrid :: Grid a -> a
-extractGrid (Compose (Cursor _ cur' _)) = extract cur'
+getCursor :: Grid a -> Cursor (Cursor a)
+getCursor (Grid c) = getCompose c
 
-updateGrid :: (a -> a) -> Grid a -> Grid a
-updateGrid f (Compose cursor) = Compose $ update (update f) cursor
+type PaddedGrid a = Grid (Maybe a)
+
+toPaddedGrid :: [[a]] -> PaddedGrid a
+toPaddedGrid = mkGrid . toCur (pure Nothing) . fmap (toCur Nothing . fmap Just)
+  where
+    toCur padding [] = Cursor (pure padding) padding (pure padding)
+    toCur padding (a : as) = Cursor (pure padding) a (rightPadded padding as)
+    rightPadded padding = foldr (:<) (pure padding)
+
+toMatrix :: PaddedGrid a -> [[a]]
+toMatrix = toList (not.null) . fmap (fmap fromJust . toList isJust)  . getCursor
+  where
+    toList p (Cursor bwd' cur' fwd') = reverse (I.takeWhile p bwd') ++ I.takeWhile p (cur':<fwd')
+
+
+data Dir = B | Z | F deriving (Show, Eq)
+
+type Dir2 = (Dir, Dir)
+
+data CardinalDir = North | East | South | West deriving (Eq, Show, Ord)
+
+toDir :: CardinalDir -> Dir2
+toDir North = (Z, B)
+toDir East = (F, Z)
+toDir South = (Z, F)
+toDir West = (B, Z)
+
+moveCursor :: Dir -> Cursor a -> Cursor a
+moveCursor B = moveBwd
+moveCursor Z = id
+moveCursor F = moveFwd
+
+moveGrid :: Dir2 -> Grid a -> Grid a
+moveGrid (h, v) = mkGrid . moveCursor v . fmap (moveCursor h) . getCursor
